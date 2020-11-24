630f33d47b42607be5e13b6869b7ff5c570046bc
Ajay Korat <ajaykorat16@gmail.com>
2020-11-24 23:03:29 +0530

    Inital commit

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NaVBar = () => {
+    return (
+        <header>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/list">Table</a></li>
+            </ul>
+        </header>
+    );
+  }
+  
+  export default NaVBar;
+  
