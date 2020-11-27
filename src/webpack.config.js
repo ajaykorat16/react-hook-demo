c37000a96a79193106a2eb2111340a659e7a2b86
Ajay Korat <ajaykorat16@gmail.com>
2020-11-27 14:56:20 +0530

    implemented webpack

---
+++ src/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_module/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+}
